bbf743846113e2f2b2b0702709a1cadf367a70dc
Faiz A. Farooqui <faizz.af@gmail.com>
2023-03-01 14:14:18 +0530

    chore: installation path fixed

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ export class GlueStackPlugin implements IPlugin, IManagesInstances, ILifeCycle {
   }
 
   getInstallationPath(target: string): string {
-    return `./${target}`;
+    return `./shared/${target}`;
   }
 
   async runPostInstall(instanceName: string, target: string) {
